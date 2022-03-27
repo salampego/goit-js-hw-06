e3eba457123bdc6c6926db1487185890d6da03a9
Haik Ivanyan <samplelogo19@gmail.com>
2022-03-27 18:32:19 +0300

    done 4 task

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+const value = document.querySelector('#value');
+const btnDecrement = document.querySelector('[data-action="decrement"]');
+const btnIncrement = document.querySelector('[data-action="increment"]');
+
+
+let counterValue = 0;
+
+btnDecrement.addEventListener("click", () => {
+    counterValue -= 1;
+    value.textContent = counterValue
+});
+btnIncrement.addEventListener("click", () => {
+    counterValue += 1;
+    value.textContent = counterValue
+});
+
+console.log ( value , btnDecrement , btnIncrement)
+
