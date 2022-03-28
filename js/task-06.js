ade17ba1cf3380e8c968591b6997a8a0309ff52d
Haik Ivanyan <samplelogo19@gmail.com>
2022-03-28 12:29:30 +0300

    done 6 task

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,11 @@
 const checkInputLength = document.querySelector('[data-length="6"]');
 
- 
-
-checkInputLength.addEventListener('blur', (event) => {
-    if (event.currentTarget.value.length <= checkInputLength.dataset.length) {
-        checkInputLength.classList.remove('invalid')
-        checkInputLength.classList.add('valid');
-    } else {
-        checkInputLength.classList.remove('valid')
-        checkInputLength.classList.add('invalid');
-    }
+checkInputLength.addEventListener('blur', event => {
+  if (event.currentTarget.value.length <= checkInputLength.dataset.length) {
+    checkInputLength.classList.remove('invalid');
+    checkInputLength.classList.add('valid');
+  } else {
+    checkInputLength.classList.remove('valid');
+    checkInputLength.classList.add('invalid');
+  }
 });
