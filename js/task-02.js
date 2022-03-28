02b880e133be5358bcbc4ed7fe69a2633f54a0f2
Haik Ivanyan <samplelogo19@gmail.com>
2022-03-28 12:29:15 +0300

    done 2 task

--- js/task-02.js
+++ js/task-02.js
@@ -7,13 +7,11 @@ const ingredients = [
   'Condiments',
 ];
 
-ingredients.forEach(function(ingredient){
+ingredients.forEach(function (ingredient) {
   const itemRef = document.createElement('li');
   itemRef.textContent = ingredient;
   itemRef.classList = 'item';
-  console.log(itemRef)
+  console.log(itemRef);
   const listRef = document.querySelector('#ingredients');
   listRef.append(itemRef);
-})
-
-
+});
