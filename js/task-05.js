486bc6d5856d8ab19edbd7b3f34fbd3a9a43df3a
Haik Ivanyan <samplelogo19@gmail.com>
2022-03-28 10:18:49 +0300

    done 5 task

---
+++ js/task-05.js
@@ -0,0 +1,17 @@
+const refs = {
+    input: document.querySelector('#name-input'),
+    spanName: document.querySelector('#name-output')
+}
+
+
+
+refs.input.addEventListener('input', (event) => {
+    refs.spanName.textContent = event.currentTarget.value
+    if (refs.input.value !== "") {
+         refs.spanName.textContent = event.currentTarget.value
+    } else {
+        refs.spanName.textContent = "Anonymous"
+    }
+})
+
+
