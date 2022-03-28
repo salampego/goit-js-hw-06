dff403bd0331aaa3959fc73a486f0a9600344b5b
Haik Ivanyan <samplelogo19@gmail.com>
2022-03-28 13:32:57 +0300

    done 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
+const body = document.body;
+const buttonBg = document.querySelector('.change-color');
+const currentBg = document.querySelector('.color');
+buttonBg.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+  currentBg.textContent = getRandomHexColor().toUpperCase();
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
