07a67731c4d61ae406b8cb6121f1b2178179e735
Haik Ivanyan <samplelogo19@gmail.com>
2022-03-28 18:22:11 +0300

    done 6 task

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const checkInputLength = document.querySelector('[data-length="6"]');
 
 checkInputLength.addEventListener('blur', event => {
-  if (event.currentTarget.value.length <= checkInputLength.dataset.length) {
+  if (event.currentTarget.value.length == checkInputLength.dataset.length) {
     checkInputLength.classList.remove('invalid');
     checkInputLength.classList.add('valid');
   } else {
