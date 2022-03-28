e4ce6b3b8d57e9ca9c6fa010c79365343c7964d3
Haik Ivanyan <samplelogo19@gmail.com>
2022-03-28 18:49:05 +0300

    done 1 task

--- js/task-01.js
+++ js/task-01.js
@@ -1,32 +1,8 @@
 const itemAllEl = document.querySelectorAll('.item');
 console.log(`Number of categories: ${itemAllEl.length}`);
 
-const categories = document.querySelector('#categories');
-// ANIMALS
-const itemAnimalsEl = categories.firstElementChild;
-const headingAnimalsEl = itemAnimalsEl.firstElementChild;
-console.log(`Category: ${headingAnimalsEl.textContent}`);
-
-const listAnimalsEl = itemAnimalsEl.lastElementChild;
-const AnimalsEl = listAnimalsEl.children;
-console.log(`Elemenst: ${AnimalsEl.length}`);
-
-// PRODUCTS
-
-const itemProductsEl = categories.children[1];
-const headingProductsEl = itemProductsEl.firstElementChild;
-console.log(`Category: ${headingProductsEl.textContent}`);
-
-const listProductsEl = itemProductsEl.lastElementChild;
-const ProductsEl = listProductsEl.children;
-console.log(`Elemenst: ${ProductsEl.length}`);
-
-// TECHONOLOGIES
-
-const itemTechonologiesEl = categories.children[2];
-const headingTechonologiesEl = itemTechonologiesEl.firstElementChild;
-console.log(`Category: ${headingTechonologiesEl.textContent}`);
-
-const listTechonologiesEl = itemTechonologiesEl.lastElementChild;
-const TechonologiesEl = listTechonologiesEl.children;
-console.log(`Elemenst: ${TechonologiesEl.length}`);
+itemAllEl.forEach(e => {
+  const ulRefs = e.children[1];
+  console.log(`Category: ${e.children[0].textContent}`);
+  console.log(`Elements: ${ulRefs.children.length}`);
+});
