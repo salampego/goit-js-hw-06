d74deac1c18e8caca586c2f97625807f9f3ba253
Haik Ivanyan <samplelogo19@gmail.com>
2022-03-28 12:29:40 +0300

    done 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget;
+  const formData = new FormData(event.currentTarget);
+
+  if (password === '' || email === '') {
+    alert('заповніть всі поля!!!');
+  } else {
+    formData.forEach((name, value) => {
+      console.log(`${value}: ${name}`);
+      event.currentTarget.reset();
+    });
+  }
+}
