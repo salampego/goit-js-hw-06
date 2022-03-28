b7881538b496bf2da16662138930cece96bc0ab5
Haik Ivanyan <samplelogo19@gmail.com>
2022-03-28 17:25:32 +0300

    done 7 task

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const controlSlider = document.querySelector('#font-size-control');
 const textHeading = document.querySelector('#text');
 
+textHeading.style.fontSize = controlSlider.value + 'px';
+
 controlSlider.addEventListener('input', () => {
   textHeading.style.fontSize = controlSlider.value + 'px';
 });
