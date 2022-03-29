7f90db49b198fa482add1567ea9c031bb1bce305
Haik Ivanyan <samplelogo19@gmail.com>
2022-03-29 10:37:52 +0300

    done 1 task

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,6 @@ const itemAllEl = document.querySelectorAll('.item');
 console.log(`Number of categories: ${itemAllEl.length}`);
 
 itemAllEl.forEach(e => {
-  const ulRefs = e.children[1];
   console.log(`Category: ${e.children[0].textContent}`);
-  console.log(`Elements: ${ulRefs.children.length}`);
+  console.log(`Elements: ${e.children[1].children.length}`);
 });
