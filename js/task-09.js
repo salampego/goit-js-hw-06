659aa548c28aac8299bb6ecfd13290fccf400b7c
Haik Ivanyan <samplelogo19@gmail.com>
2022-04-02 13:12:58 +0300

    исправил ошибки в 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -2,10 +2,10 @@ const body = document.body;
 const buttonBg = document.querySelector('.change-color');
 const currentBg = document.querySelector('.color');
 buttonBg.addEventListener('click', () => {
-  body.style.backgroundColor = getRandomHexColor();
-  currentBg.textContent = getRandomHexColor().toUpperCase();
+  const colorChanger = (body.style.backgroundColor = `#` + getRandomHexColor());
+  currentBg.textContent = colorChanger.toUpperCase();
 });
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return Math.floor(Math.random() * 16777215).toString(16);
 }
